72ed397a1b9e8301eab9501c19b88ee5bae9978f
Hitesh Kuber <hiteshkuber@gmail.com>
2023-08-25 00:17:15 +0530

    Addded circular slider, heavily inspired by yt K Apps.

--- app/src/main/java/com/example/flashmob/MainActivity.kt
+++ app/src/main/java/com/example/flashmob/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.flashmob
 
-import CustomSlider
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.flashmob.ui.theme.FlashMobTheme
+import com.example.flashmob.ui.theme.darkGray
+import com.example.flashmob.ui.theme.green
 import kotlin.properties.ObservableProperty
 
 class MainActivity : ComponentActivity() {
@@ -25,34 +32,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FlashMobTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(darkGray),
+                    contentAlignment = Alignment.Center
                 ) {
-                    CustomSlider(curr, ::thisii)
+
+                    CustomCircularProgressIndicator(
+                        initialValue= 50,
+                        primaryColor= green,
+                        secondaryColor= Color.Yellow,
+                        circleRadius = 250f,
+                        onPositionChange = { position ->
+                            //Do something with position
+
+                        },
+                        modifier = Modifier
+                            .size(250.dp)
+                            .background(darkGray)
+                    )
                 }
+
             }
         }
     }
-    fun thisii(i: Float) {
-        curr = i
-    }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlashMobTheme {
-        Greeting("Android")
-    }
 }
